89d29b7523f2295f2adefacdde58329e420d0049
Waether <nathan.hautbois@epitech.eu>
2019-11-05 16:29:48 +0100

    added Move comment

--- includes/Move.hpp
+++ includes/Move.hpp
@@ -6,14 +6,14 @@
 #define PHANTOMOPERAAI_MOVE_HPP
 
 typedef struct      s_Move {
-    unsigned int    _characterIdx;
-    unsigned int    _positionIdx;
-    unsigned int    _activatePowerIdx;
-    unsigned int    _handlePowerIdx;
-    unsigned int    _powerExitIdx;
-    unsigned int    _powerRoomIdx;
-    unsigned int    _powerMoveIdx;
-    unsigned int    _powerMoveTargetIdx;
+    unsigned int    _characterIdx; // 0 - 3 NEED CHECK
+    unsigned int    _positionIdx; // 0 - 9 NEED CHECK
+    unsigned int    _activatePowerIdx; // 0 - 1
+    unsigned int    _handlePowerIdx; // NEED CHECK
+    unsigned int    _powerExitIdx; // NEED CHECK
+    unsigned int    _powerRoomIdx; // 0 - 9 NEED CHECK
+    unsigned int    _powerMoveIdx; // NEED CHECK
+    unsigned int    _powerMoveTargetIdx; // NEED CHECK
 }                   Move;
 
 #endif //PHANTOMOPERAAI_MOVE_HPP
